291ad12bf43e31201d489ea592ad1b9d57dec8c2
Avinash <aviesdfsdfsdnash.kumar@instarem.com>
2020-12-07 11:57:05 +0530

    fetch grid added

--- src/containers/Gallery.js
+++ src/containers/Gallery.js
@@ -1,25 +1,71 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { getPhotos } from '../api/getPhotos';
 import { searchPhotos } from '../api/searchPhotos';
 import { InputElement } from '../components';
 import { useAsync } from '../useFetch';
 
 const Section = styled.section`
-    >input{
-        margin: 20px 0;
-        min-height: 50px; 
+  > input {
+    margin: 20px 0;
+    min-height: 25px;
+  }
+
+  .gallery {
+    display: grid;
+    grid-template-columns: repeat(8, 1fr);
+    grid-template-rows: repeat(8, 5vw);
+    grid-gap: 15px;
+    > figure {
+      grid-column: span 4;
+      grid-row: span 4;
+      > img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
     }
-`
+  }
+`;
 export const Gallery = () => {
-  const {execute, status, value, error} = useAsync(searchPhotos)
+  const [searchValue, setSearchValue] = useState('')
+  const [photosParam, setPhotosParam] = useState({
+    count:null,
+    start:null
+  })
+  const {execute: fetchPhotos, status:statusPhotos, value:photos, error: photoError} = useAsync(getPhotos)
+  const {execute: searchPhotos, status:statusSearch, value: searchResponse, error: photosError} = searchValue && useAsync(searchPhotos)
 
   useEffect(()=>{
-    execute('office')
-  },[])
+    if(searchValue){
+      execute("office");
+    }
+  },[searchValue])
+
+  // useEffect(() => {
+  //   execute()
+  // }, [])
   return (
     <Section>
       {/* <h3>Gallery</h3> */}
       <InputElement placeholder="Search Images" />
+      <div className="gallery">
+        {statusPhotos === "success" &&
+          photos.map((images, i) => (
+            <figure
+              className={`gallery__item gallery__item--${i*i}`}
+              key={images.id}
+
+            >
+              <img
+                className="gallery__img"
+                key={images.id}
+                src={images.urls.thumb}
+                alt={images.user.name}
+              />
+            </figure>
+          ))}
+      </div>
     </Section>
   );
 };
