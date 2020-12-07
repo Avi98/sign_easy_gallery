e21f3e026347f9ed1a2fbeb947cf1c4ee2f83cad
Avinash <aviesdfsdfsdnash.kumar@instarem.com>
2020-12-07 13:19:40 +0530

    like dislike functionlity added

--- src/containers/Gallery.js
+++ src/containers/Gallery.js
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { getPhotos } from '../api/getPhotos';
 import { searchPhotos } from '../api/searchPhotos';
-import { InputElement } from '../components';
+import { InputElement, LikeDislikeButtons } from '../components';
 import { useAsync } from '../useFetch';
+
 
 const Section = styled.section`
   > input {
@@ -17,6 +18,7 @@ const Section = styled.section`
     grid-template-rows: repeat(8, 5vw);
     grid-gap: 15px;
     > figure {
+      position:relative;
       cursor: pointer;
       grid-column: span 4;
       grid-row: span 4;
@@ -28,12 +30,21 @@ const Section = styled.section`
     }
   }
 `;
+
+const isClickedInitialState = {
+  id:'',
+  name:'',
+  isClicked:false
+}
 export const Gallery = () => {
   const [searchValue, setSearchValue] = useState('')
   const [photosParam, setPhotosParam] = useState({
     count:null,
     start:null
   })
+  const [like, setLiked] = useState(isClickedInitialState)
+  const [disLike, setDisLike] = useState(isClickedInitialState)
+
   const {execute: fetchPhotos, status:statusPhotos, value:photos, error: photoError} = useAsync(getPhotos)
   const {execute: searchPhotos, status:statusSearch, value: searchResponse, error: photosError} = searchValue && useAsync(searchPhotos)
 
@@ -43,9 +54,31 @@ export const Gallery = () => {
     }
   },[searchValue])
 
+  const onClickDislike = ({id, name})=>{
+    debugger
+    if(disLike.id !== id && !like.id){
+      setDisLike({id, name, isClicked:true})
+    }else if(like.id){
+      setLiked(isClickedInitialState)
+    }
+    else{
+      setDisLike(isClickedInitialState)
+    }
+  } 
+  const onClickLike = ({id, name})=>{
+    if(like.id !== id ){
+      setLiked({id, name, isClicked:true})
+    }else if(disLike.id){
+      setDisLike(isClickedInitialState)
+    }
+    else{
+      setLiked(isClickedInitialState)
+    }
+  } 
   // useEffect(() => {
   //   execute()
   // }, [])
+
   return (
     <Section>
       {/* <h3>Gallery</h3> */}
@@ -53,18 +86,31 @@ export const Gallery = () => {
       <div className="gallery">
         {statusPhotos === "success" &&
           photos.map((images, i) => (
-            <figure
-              className={`gallery__item gallery__item--${i*i}`}
-              key={images.id}
-
-            >
-              <img
-                className="gallery__img"
+            <>
+              <figure
+                className={`gallery__item gallery__item--${i * i}`}
                 key={images.id}
-                src={images.urls.thumb}
-                alt={images.user.name}
-              />
-            </figure>
+              >
+                <img
+                  className="gallery__img"
+                  key={images.id}
+                  src={images.urls.thumb}
+                  alt={images.user.name}
+                />
+                <LikeDislikeButtons
+                  liked={like.id === images.id}
+                  disliked={disLike.id === images.id}
+                  onClickDislike={()=>onClickDislike({
+                    id: images.id,
+                    name: images.user.name,
+                  })}
+                  onClickLike={()=>onClickLike({
+                    id: images.id,
+                    name: images.user.name,
+                  })}
+                />
+              </figure>
+            </>
           ))}
       </div>
     </Section>
